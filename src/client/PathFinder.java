8ffaef1d17ee688851525ddf16b454f9f602fa23
Erick <erick.lauridsen@gmail.com>
2019-06-06 15:14:48 +0200

    Created class PathFinder

---
+++ src/client/PathFinder.java
@@ -0,0 +1,11 @@
+package client;
+
+import java.util.ArrayList;
+
+public class PathFinder {
+	RobotLocation robotLocation;
+	MapState mapState;
+	
+	public ArrayList<Route> getCalculatedRoutesBalls(MapState mapState);
+
+}
