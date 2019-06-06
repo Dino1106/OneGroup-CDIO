164410a4a2ad85e5ff29a40c6cd075069ae177da
Sebastian Stokkebro <s170423@student.dtu.dk>
2019-06-06 15:18:29 +0200

    added drive and rotate functionality

--- src/server/RobotMovement.java
+++ src/server/RobotMovement.java
@@ -1,23 +1,56 @@
 package server;
 
+import lejos.robotics.geometry.Point;
 import client.Coordinate;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.robotics.chassis.*;
+import lejos.robotics.localization.OdometryPoseProvider;
+import lejos.robotics.localization.PoseProvider;
+import lejos.robotics.navigation.*;
 
 public class RobotMovement { 
 	 
 	public static final int port = 1337;
-	final UnregulatedMotor ball_picker_left 	= new UnregulatedMotor(MotorPort.C); 
-    final UnregulatedMotor ball_picker_right   	= new UnregulatedMotor(MotorPort.D);
-	
-	public void drive(Coordinate from, Coordinate to) {
+	private UnregulatedMotor ball_picker_left, ball_picker_right;
+	private Wheel left_wheel, right_wheel;
+    private Chassis chassis;
+    private MovePilot pilot;
+    private PoseProvider poseProvider;
+    private Navigator navigator;
+	private double wheelDiameter;
+    
+    
+    public RobotMovement() {
+    	/* Setup ball-picker motors */
+    	this.ball_picker_left = new UnregulatedMotor(MotorPort.C); 
+        this.ball_picker_right = new UnregulatedMotor(MotorPort.D);
+        
+        /* Setup wheels and chassis for pilot */
+    	this.wheelDiameter = 6.88;
+    	this.left_wheel = WheeledChassis.modelWheel(Motor.A, wheelDiameter);
+    	this.right_wheel = WheeledChassis.modelWheel(Motor.B, wheelDiameter);
+    	this.chassis = new WheeledChassis(new Wheel[] {left_wheel, right_wheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+ 
+    	/* Setup navigator with pilot */
+        this.pilot = new MovePilot(chassis); 
+        this.navigator = new Navigator(pilot);
+        this.poseProvider = new OdometryPoseProvider(pilot);
+    }
+    
+	public void drive(Coordinate to) {
+		// TODO: Maybe use coordinate from as method parameter instead of pose
 		
+		Pose pose = poseProvider.getPose();				// Gets the 'pose' of robot (position and heading)
+		Point location = pose.getLocation();
+		
+		navigator.goTo(new Waypoint(to.x, to.y));
 		
 	}
 	
-	public void rotateToOrientation(int degrees) {
-		
-		
+	public void rotateToOrientation(double degrees) {
+		pilot.rotate(degrees);
 	}
 	
 	public void pickUpBalls(boolean pickUp) {
