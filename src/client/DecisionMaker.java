3e407d5d678bc800fc7d88a6a3cc733dd83886a4
Erick <erick.lauridsen@gmail.com>
2019-06-06 13:47:06 +0200

    Auto stash before rebase of "feature/106-DecisionMaker"

---
+++ src/client/DecisionMaker.java
@@ -0,0 +1,24 @@
+package client;
+
+import java.util.ArrayList;
+
+public class DecisionMaker {
+	
+	MapState mapState;
+	ArrayList<Route> routes;
+	int ballCount;
+	
+	
+	public void MainLoop() {
+		
+	}
+	
+	public Route choosePath() {
+			
+	}
+	
+	public int countBalls() {
+		
+	}
+
+}
