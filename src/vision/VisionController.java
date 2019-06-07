d41f687cf4b2b8e89ac698240dfc6694cd0c7661
Jakub Tomczak <jaycobe9@gmail.com>
2019-06-07 12:51:50 +0200

    General cleanup of VisionController.java

--- src/vision/VisionController.java
+++ src/vision/VisionController.java
@@ -2,7 +2,6 @@ package vision;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2GRAY;
 import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2HSV;
-import static org.bytedeco.opencv.global.opencv_imgproc.CV_HOUGH_GRADIENT;
 import static org.bytedeco.opencv.global.opencv_imgproc.HoughCircles;
 import static org.bytedeco.opencv.global.opencv_imgproc.circle;
 import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
@@ -12,7 +11,6 @@ import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
-import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_core.Scalar;
@@ -25,7 +23,7 @@ import org.bytedeco.opencv.opencv_core.*;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
 /**
- * @author Jakub Tomczak
+ * @author OneGroup
  *
  */
 
@@ -50,10 +48,9 @@ public class VisionController implements Runnable {
 	private Vec3fVector Circle_set = new Vec3fVector();
 	private int Camera_id;
 	private Mat picture_global = new Mat(), picture_plain = new Mat();
-	private Mat edges_global;
 	private boolean vid;
 
-	// Laptop camera, TODO: Delete later.
+	// Laptop camera constructor
 	public VisionController() {
 		Camera_id = 0;
 		vid_frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
@@ -61,40 +58,48 @@ public class VisionController implements Runnable {
 	}
 
 	// USB plugged camera.
-	// Int camera is the ID of the camera, can be made constant.
+	// ID of the camera indicates which camera has to be used
 	public VisionController(int camera) {
 		Camera_id = camera;
 		vid_frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 		vid = true;
 	}
 
-	// If you want to test with a static image, TODO: Maybe delete later.
+	// Constructor with a static image
 	public VisionController(String imgpath) {
 		Camera_id = 0;
 		vid_frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 		vid = false;
 		picture_global = imread(imgpath);
+
 	}
 
+
+	// Code to execute in the class:
 	@Override
 	public void run() {
 		try {
+			// Controller Initializers
 			FrameGrabber grabber = FrameGrabber.createDefault(Camera_id);
 			OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
-			
 			grabber.setImageHeight(imageHeight);
 			grabber.setImageWidth(imageWidth);
-			
-			//converter.convert(grabber.grab());
+
+			if (vid) picture_global = converter.convert(grabber.grab());
+
+			// Set Calibration values for Identify Balls
 			int[] calib = {6, 5, 2, 6, 20};
+
+			// Generate layers
 			extract_layer(picture_global);
+
+			// 1 - Identify balls with given parameters and draw circles
 			IdentifyBalls identifyBalls = new IdentifyBalls(picture_plain, 1, 3, 120, 15, 2, 8, calib);
 			draw_circles(false, identifyBalls.get_circle());
-			for (int i = 0; i < 3; i++) {
-				System.out.println("X is: " + identifyBalls.get_circle().get(i).get(0));
-				System.out.println("Y is: " + identifyBalls.get_circle().get(i).get(1));
-			}
 
+
+
+			// Update window frame with current picture frame
 			vid_frame.showImage(converter.convert(get_pic()));
 	 		vid_edges.showImage(converter.convert(get_plain()));
 			
@@ -103,29 +108,19 @@ public class VisionController implements Runnable {
 		}
 	}
 
+	// Creates lines between all circles
 	public void create_nodes() {
 		int i;
 		int u;
-		for (u = 0; u < get_vec_len(); u++)
-			for (i = 0; i < get_vec_len(); i++)
+		for (u = 0; u < get_Circles_amount(); u++)
+			for (i = 0; i < get_Circles_amount(); i++)
 				if (i != u)
-					line(get_pic(), new Point(get_circle(u, x_circle), get_circle(u, y_circle)),
-							new Point(get_circle(i, x_circle), get_circle(i, y_circle)), Scalar.MAGENTA);
+					line(get_pic(), new Point(get_circle_xyr(u, x_circle), get_circle_xyr(u, y_circle)),
+							new Point(get_circle_xyr(i, x_circle), get_circle_xyr(i, y_circle)), Scalar.MAGENTA);
 	}
 
-	/**
-	 * src_gray: Input image (grayscale) circles: A vector that stores sets of 3
-	 * values: x_{c}, y_{c}, r for each detected circle. CV_HOUGH_GRADIENT: Define
-	 * the detection method. Currently this is the only one available in OpenCV dp =
-	 * 1: The inverse ratio of resolution min_dist = src_gray.rows/8: Minimum
-	 * distance between detected centers param_1 = 200: Upper threshold for the
-	 * internal Canny edge detector param_2 = 100*: Threshold for center detection.
-	 * min_radius = 0: Minimum radio to be detected. If unknown, put zero as
-	 * default. max_radius = 0: Maximum radius to be detected. If unknown, put zero
-	 * as default
-	 *
-	 */
 
+	// Takes Value layer and generates single-layered Mat
 	private void extract_layer(Mat picture) {
 		BytePointer dat;
 		
@@ -143,58 +138,33 @@ public class VisionController implements Runnable {
 		
 	}
 	
-	/*
-	private void extract_layer() {
-		Mat picture = get_pic();
-		BytePointer dat;
 
-		cvtColor(picture, picture, COLOR_BGR2HSV);
-		dat = picture.data();
-
-		for (int i = 0; i < (picture.arrayHeight() * picture.arrayWidth() * 3); i += 3) {
-			dat = dat.put(0 + i, (byte) dat.get(i + 2));
-			dat = dat.put(1 + i, (byte) dat.get(i + 2));
-		}
-		// picture = picture.data(dat);
-
-		cvtColor(picture, picture, COLOR_BGR2GRAY);
-		picture_plain = picture_global.clone();
-		cvtColor(picture, picture, COLOR_GRAY2BGR);
-	}
-	*/
-	
-	private void extract_circles(int resolution_ratio, int min_distance, int Canny_threshold, int Center_threshold,
-			int min_rad, int max_rad) {
-		Vec3fVector circle = new Vec3fVector();
-		HoughCircles(get_plain(), circle, CV_HOUGH_GRADIENT, resolution_ratio, min_distance, Canny_threshold,
-				Center_threshold, min_rad, max_rad);
-		toVec(circle);
-	}
-
+// TODO: THE METHOD NEEDS TO BE IMPLEMENTED IN IDENTIFY_CROSS.java
 	private void extract_lines(double rho, double theta, int threshold, int minLineLength, int maxLineGap,
-			Size filter_dim, int threshold1, int threshold2) {
+		Size filter_dim, int threshold1, int threshold2) {
 		Vec4iVector lines = new Vec4iVector();
 		Mat blurred = new Mat(), edges = new Mat();
 		blur(get_plain(), blurred, filter_dim);
 		Canny(blurred, edges, threshold1, threshold2);
 		HoughLinesP(edges, lines, rho, theta, threshold, minLineLength, maxLineGap);
-		to_lineVec(lines);
+		Line_set = lines;
 		draw_lines();
 	}
 
-	public synchronized int get_circle(int circle_number, int parameter) {
+
+	public synchronized int get_circle_xyr(int circle_number, int parameter) {
 		return (int) Circle_set.get(circle_number).get(parameter);
 	}
 
-	public synchronized int get_line(int line_number, int parameter) {
+	public synchronized int get_line_xyxy(int line_number, int parameter) {
 		return new IntPointer(Line_set.get(line_number)).get(parameter);
 	}
 
-	public synchronized int get_vec_len() {
+	public synchronized int get_Circles_amount() {
 		return (int) Circle_set.size();
 	}
 
-	public synchronized int get_Linevec_len() {
+	public synchronized int get_Lines_amount() {
 		return (int) Line_set.size();
 	}
 
@@ -206,126 +176,45 @@ public class VisionController implements Runnable {
 		return picture_plain;
 	}
 
-	public synchronized Mat get_edges() {
-		return edges_global;
-	}
 
-	/*
-	 * private void calculate_nodes() {
-	 * 
-	 * int i;
-	 * 
-	 * Vector<Vector<Point>> temp_v = new Vector<Vector<Point>>(); Vector<Point>
-	 * temp_dat = new Vector<Point>();
-	 * 
-	 * for(i = 0; i < get_vec_len()-1;i++) { temp_dat.add(new Point(get_circle(i,
-	 * 0),get_circle(i,1))); temp_dat.add(new Point(get_circle(i+1,
-	 * 0),get_circle(i+1,1))); temp_v.add(temp_dat); temp_dat.clear(); } }
-	 */
 
-	private synchronized void toVec(Vec3fVector vec) {
-		Circle_set = vec;
-	}
+
+	//private synchronized void toVec(Vec3fVector vec) {
+	//	Circle_set = vec;
+	//}
+
+
 
 	private synchronized void draw_circles(Boolean centers, Vec3fVector ballCoords) {
 		for (int i = 0; i < 7; i++) {
 			circle(get_pic(), new Point((int) ballCoords.get(i).get(x_circle), (int) ballCoords.get(i).get(y_circle)), (int) ballCoords.get(i).get(rad_circle),
 					Scalar.RED);
 			if (centers) {
-				line(get_pic(), new Point(get_circle(i, x_circle) - 3, get_circle(i, y_circle)),
-						new Point(get_circle(i, x_circle) + 3, get_circle(i, y_circle)), Scalar.BLUE);
-				line(get_pic(), new Point(get_circle(i, x_circle), get_circle(i, y_circle) - 3),
-						new Point(get_circle(i, x_circle), get_circle(i, y_circle) + 3), Scalar.BLUE);
+				line(get_pic(), new Point(get_circle_xyr(i, x_circle) - 3, get_circle_xyr(i, y_circle)),
+						new Point(get_circle_xyr(i, x_circle) + 3, get_circle_xyr(i, y_circle)), Scalar.BLUE);
+				line(get_pic(), new Point(get_circle_xyr(i, x_circle), get_circle_xyr(i, y_circle) - 3),
+						new Point(get_circle_xyr(i, x_circle), get_circle_xyr(i, y_circle) + 3), Scalar.BLUE);
 			}
 		}
 	}
 
 	private void draw_lines() {
-		for (int i = 0; i < get_Linevec_len(); i++) {
-			line(get_pic(), new Point(get_line(i, xstart_line), get_line(i, ystart_line)),
-					new Point(get_line(i, xend_line), get_line(i, yend_line)), Scalar.RED);
+		for (int i = 0; i < get_Lines_amount(); i++) {
+			line(get_pic(), new Point(get_line_xyxy(i, xstart_line), get_line_xyxy(i, ystart_line)),
+					new Point(get_line_xyxy(i, xend_line), get_line_xyxy(i, yend_line)), Scalar.RED);
 		}
 
 		// System.out.println(new IntPointer(Line_set.get(0)).get(0));
 
 	}
 
-	private synchronized void to_lineVec(Vec4iVector vec) {
-		Line_set = vec;
-	}
 
-	/*
-	 * private synchronized void draw_lines() { HoughCircles(get_pic(), , arg2,
-	 * arg3, arg4, arg5, arg6, arg7, arg8);
-	 * 
-	 * 
-	 * }
-	 */
 
-	private synchronized void update_image(Mat img, Mat edg) {
-		picture_global = img;
-		edges_global = edg;
-	}
 
-	private synchronized void update_image(Mat img) {
-		picture_global = img;
-	}
 
-	private void Generate_Objects(Mat picture) {
-		extract_layer(picture);
-		// extract_lines( 1, CV_PI/180, 30, 0, 200, new Size(3,3), 50, 100);
-		// extract_circles(1,50,120,80,50,100);
-		//auto_circle(3, 120, 15, 2, 8);
-	}
+
 /*
-	private void auto_circle(int param1, int param2, int param3, int param4, int param5) {
-		int max_change_param1 = 6;
-		int max_change_param2 = 5;
-		int max_change_param3 = 2;
-		int max_change_param4 = 6;
-		int max_change_param5 = 20;
-		int amount_circles = 7;
-
-		int sec1, sec2 = param2, sec3 = param3, sec4 = param4, sec5 = param5;
-		outerloop: do {
-			for (sec1 = param1 ; sec1 <= param1 + max_change_param1; sec1++) {
-				extract_circles(1, sec1, sec2, sec3, sec4, sec5);
-				if (eval(amount_circles))
-					break outerloop;
-				for (sec2 = (param2 - max_change_param2); sec2 <= param2 + max_change_param2; sec2++) {
-					extract_circles(1, sec1, sec2, sec3, sec4, sec5);
-					if (eval(amount_circles))
-						break outerloop;
-					for (sec3 = (param3 - max_change_param3); sec3 <= param3 + max_change_param3; sec3++) {
-						extract_circles(1, sec1, sec2, sec3, sec4, sec5);
-						if (eval(amount_circles))
-							break outerloop;
-						for (sec4 = (param4); sec4 <= param4 + max_change_param4; sec4++) {
-							extract_circles(1, sec1, sec2, sec3, sec4, sec5);
-							if (eval(amount_circles))
-								break outerloop;
-							for (sec5 = (param5); sec5 <= param5 + max_change_param5; sec5++) {
-								extract_circles(1, sec1, sec2, sec3, sec4, sec5);
-								if (eval(amount_circles))
-									break outerloop;
-							}
-						}
-					}
-				}
-
-			}
-		} while (!eval(amount_circles--));
-		draw_circles(true);
-		create_nodes();
-
-	}*/
-
-	private boolean eval(int amount) {
-		if (get_vec_len() == amount)
-			return true;
-		return false;
-	}
-
+// TODO: Do not delete comments below
 	/*
 	 * @Override public void run() { try {
 	 * 
