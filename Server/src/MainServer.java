6bbb88320a2034f5376b71ccc1b5072281d3526d
Sebastian Stokkebro <s170423@student.dtu.dk>
2019-06-12 13:36:24 +0200

    deleted useless code

--- Server/src/MainServer.java
+++ Server/src/MainServer.java
@@ -1,12 +1,11 @@
 
 import java.io.DataInputStream;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainServer extends Thread {
 	
@@ -117,17 +116,6 @@ public class MainServer extends Thread {
 				default:
 					break;
 				}
-				
-				/*boolean pickUp = (boolean) dIn.readObject();
-				carPickUpBalls(pickUp);
-				
-				int speed = (int) dIn.readObject();
-				setMotorSpeed(speed);*/
-				
-				/*String coordinateObject = (String) dIn.readObject();
-				String[] coordArray = coordinateObject.split(" ");
-				Coordinate coordinate = new Coordinate(Integer.parseInt(coordArray[0]), Integer.parseInt(coordArray[1]));
-				carDrive(coordinate);*/
 			}
 			
 		} catch (IOException e) {
