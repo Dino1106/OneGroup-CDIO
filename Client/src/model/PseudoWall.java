5ccd3722b180e1f85251e7433c814895fa1e7807
Erick <erick.lauridsen@gmail.com>
2019-06-13 10:46:50 +0200

    Implemented a wall interface

--- Client/src/model/PseudoWall.java
+++ Client/src/model/PseudoWall.java
@@ -1,7 +1,7 @@
 package model;
 
 // A pseudo-wall model to be used for creating upper and lower walls.
-public class PseudoWall {
+public class PseudoWall implements InterfaceWall {
 	
 	public Coordinate left;
 	public Coordinate right;
